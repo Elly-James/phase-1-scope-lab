5d67b41f7c0775b1c282e4201e60259175c0c668
Elly <ellyjames1999@gmail.com>
2024-11-30 10:22:17 +0300

    Finished test

--- mine.js
+++ mine.js
@@ -1,22 +1,41 @@
 // Write your solution in this file!
 
+
 var customerName='bob';
+
+console.log(customerName);
+
 
 function upperCaseCustomerName(){
     customerName = customerName.toUpperCase();
+
+    
+
 }
 
 
+
+
 function setBestCustomer(){
-    bestCustomer='not bob'; 
+    bestCustomer='not bob';
+
+   
  }
+
+ console.log(setBestCustomer());
+
+
 
  function overwriteBestCustomer(){
      bestCustomer='maybe bob';
+     return bestCustomer;
+
  }
 
- const  leastFavoriteCustomer='Rasto'
+ console.log(overwriteBestCustomer());
 
+
+ const  leastFavoriteCustomer='Rasto'
  function changeLeastFavoriteCustomer(){
     leastFavoriteCustomer= 'Gusto';
  }
